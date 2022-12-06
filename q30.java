dbac031ebd6c0a4c4f0fa24790a0289a06820300
Uttamasha <115366512+Uttamasha@users.noreply.github.com>
2022-12-06 19:28:03 +0530

    Update 30.java

--- q30.java
+++ q30.java
@@ -5,9 +5,6 @@ public final class q30{
 
 	
 	public String run() {
-		// As stated in the problem, 1 = 1^5 is excluded.
-		// If a number has at least n >= 7 digits, then even if every digit is 9,
-		// n * 9^5 is still less than the number (which is at least 10^n).
 		int sum = 0;
 		for (int i = 2; i < 1000000; i++) {
 			if (i == fifthSum(i))
